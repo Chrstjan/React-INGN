48c1ed24e3322f1852cfb98b10d3567d7e7d4a9c
Christian <ps5christian@hotmail.com>
2024-10-02 10:29:29 +0200

    Made more desktop card styling + tried the grid display

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,49 @@
 import { Link } from "react-router-dom";
 import Markdown from "markdown-to-jsx";
 import style from "./Card.module.scss";
+import { useEffect } from "react";
 
 export const Card = ({ data, hideReadMore, cardStyling }) => {
+  // const desktopGrid = [
+  //   style.news1,
+  //   style.news2,
+  //   style.news3,
+  //   style.news4,
+  //   style.news5,
+  //   style.news6,
+  //   style.news7,
+  //   style.news8,
+  //   style.news9,
+  // ];
+
+  // const createGridArray = (_arr) => {
+  //   let newArray = [[]];
+
+  //   _arr.forEach((item, index) => {
+  //     length = newArray.length;
+  //     if ((index + 1) % 9) {
+  //       newArray[length - 1].push(item);
+  //     } else {
+  //       newArray.push([]);
+  //       newArray[length - 1].push(item);
+  //     }
+  //   });
+
+  //   console.log(newArray);
+  // };
+
+  // useEffect(() => {
+  //   createGridArray(data);
+  // }, [data]);
+
   return (
     <>
-      {data?.map((item) => {
+      {data?.slice(0, 9).map((item, index) => {
         return (
           <figure
             key={item.title}
             className={`${style.cardStyling} ${style[cardStyling]}`}
+            // style={{ gridArea: "news" + (index + 1) }}
           >
             <div className={style.textContainer}>
               <header>
@@ -32,7 +66,7 @@ export const Card = ({ data, hideReadMore, cardStyling }) => {
                 </span>
               </figcaption>
             </div>
-            <div>
+            <div className={style.thumbnailContainer}>
               <img
                 className={style.thumbnailStyling}
                 src={item.images[0].url}
